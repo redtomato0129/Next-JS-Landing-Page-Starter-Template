92a7c23a373a0559ad320fec58637a192865fc7d
redtomato0129 <redtomato0129@gmail.com>
2024-04-22 21:46:30 -0400

    fix bug

--- src/templates/Banner.tsx
+++ src/templates/Banner.tsx
@@ -10,7 +10,7 @@ const Banner = () => (
       title="Lorem ipsum dolor sit amet consectetur adipisicing elit."
       subtitle="Start your Free Trial."
       button={
-        <Link href="https://creativedesignsguru.com/category/nextjs/">
+        <Link href="https://superman921030-next-landing-template.vercel.app/category/nextjs/">
           <Button>Get Started</Button>
         </Link>
       }
